8da7e5e5094e4bef1e29b9b13a2b3066f08404ac
arundalep <56804410+arundalep@users.noreply.github.com>
2026-05-30 11:29:42 +0100

    Add files via upload

---
+++ esphome-m5-ain-4-20ma/m5_ain_4_20ma/sensor.py
@@ -0,0 +1,9 @@
+"""M5Stack AIN 4-20mA Sensor Component
+
+Automatically loaded by __init__.py - no additional configuration needed.
+
+This file is part of the ESPHome external component structure.
+The actual sensor implementation is in sensor.cpp
+"""
+
+__all__ = []
